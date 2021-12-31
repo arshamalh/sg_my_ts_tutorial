083575ec2c4fd6970c02652c2db70d032188f435
arshamalh <arshamalh@gmail.com>
2021-12-31 11:24:45 +0330

    Markers and info window added.

--- src/Map.ts
+++ src/Map.ts
@@ -1,14 +1,39 @@
 /// <reference types="@types/google.maps" />
-export class Map {
-    googleMap: google.maps.Map
 
-    constructor() {
-        this.googleMap = new google.maps.Map(document.getElementById("map"), {
-            zoom: 1
-            center: {
-                lat:,
-                lng:
-            }
-        })
-    }
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class Map {
+  private googleMap: google.maps.Map;
+
+  constructor(divID: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divID), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable) {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable.location,
+      // position: {
+      //     lat: mappable.location.lat,
+      //     lng: mappable.location.lng
+      // }
+    });
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi there!",
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { User } from "./User";
 import { Company } from "./Company";
+import { Map } from "./Map";
 
+let user = new User();
+let company = new Company();
+let map = new Map("map");
+map.addMarker(user);
+map.addMarker(company);
